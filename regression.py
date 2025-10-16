99d357e44f88cf5df995ab1fc7e0747ad2041bda
TheMoJd <moetez.jaoued@etudiant.univ-brest.fr>
2025-10-16 13:05:09 +0200

    Enhance regression model with polynomial features and MinMax scaling

--- regression.py
+++ regression.py
@@ -22,3 +22,28 @@ y_pred_test = reg.predict(X_test)
 from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 print(f"RMSE: {mean_squared_error(y_test, y_pred_test)}")
 print(f"MAPE: {mean_absolute_percentage_error(y_test, y_pred_test)}")
+
+# pour améliroer les résultats on va faire la régression polynomiale
+
+df['tv2'] = df.tv**2    
+
+print(df)
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaler.fit(df) # rends les valeures entre 0 et 1
+data_array = scaler.transform(df)
+df = pd.DataFrame(data_array, columns = ['tv','radio','journaux','ventes','tv2'])
+print(df)
+print(df.describe().loc[['min','max']])
+X = df[['tv','radio','journaux', 'tv2']]
+y = df.ventes
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+reg.fit(X_train, y_train)
+y_hat_test = reg.predict(X_test)
+
+print(f"Coefficients: {reg.coef_}")
+print(f"RMSE: {mean_squared_error(y_test, y_hat_test)}")
+print(f"MAPE: {mean_absolute_percentage_error(y_test, y_hat_test)}")
